d21038e97aac13eae6e6fdf1157f52afc375b40c
José Mota <jose.a.mota11@gmail.com>
2023-03-19 16:37:41 -0400

    :sparkles: Adding Navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,29 @@
+import { useRef } from 'react';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const hamburgerRef = useRef<HTMLButtonElement>(null);
+
+  const handleClick = () => {
+    hamburgerRef.current?.classList.toggle('active');
+  };
+  return (
+    <nav className='navbar'>
+      <h1 className='navbar-logo'>Game Store 3.0</h1>
+      <input
+        className='navbar-input'
+        type='search'
+        placeholder='E.g. Minecraft'
+      />
+      <button className='navbar-button'>Login</button>
+      <button
+        className='navbar-hamburger'
+        ref={hamburgerRef}
+        onClick={handleClick}
+      >
+        <span className='navbar-hamburger__bar'></span>
+      </button>
+    </nav>
+  );
 };
 
 export default Navbar;
